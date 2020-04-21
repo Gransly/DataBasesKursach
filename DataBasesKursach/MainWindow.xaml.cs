3cdcf39aa9020ffa528373027e116cfd4ece3162
Gransly <bob031299@gmail.com>
2020-04-21 13:33:02 +1000

    Доработана login страница, добавлены кнопки выхода из приложения

--- DataBasesKursach/MainWindow.xaml.cs
+++ DataBasesKursach/MainWindow.xaml.cs
@@ -30,10 +30,12 @@ namespace DataBasesKursach
 
         private void Button_Click(object sender, RoutedEventArgs e)                     //login to db method
         {
-            string connString = "Server=localhost;Database=logindb;port=3306;User Id=" + userBox.Text + ";password=" + pasBox.Password;
+            string connString = "Server=127.0.0.1;Database=kusrchsheme;port=3306;User Id=" + userBox.Text + ";password=" + pasBox.Password;
             MySqlConnection connection = new MySqlConnection(connString);
             try 
             {
+                //Window1 window1 = new Window1();
+                //window1.Show();
                 connection.Open();
             }
             catch (Exception ex)
@@ -43,9 +45,38 @@ namespace DataBasesKursach
             }
             finally 
             {
-                this.Close();
-                connection.Close();
+                if(connection.State == System.Data.ConnectionState.Open) 
+                {
+                    Window1 window1 = new Window1();
+                    window1.Show();
+                    this.Close();
+                    
+                }
             }
+
+            
+
+            
+        }
+
+        private void userBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            userBox.Clear();
+        }
+
+        private void pasBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            pasBox.Clear();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
         }
     }
 }
---
+++ DataBasesKursach/Window1.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DataBasesKursach
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+    }
+}
